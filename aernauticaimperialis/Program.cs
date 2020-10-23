406163417544b427a9298a71e255127e0b677d95
Julia <j.schenk@htlkrems.at>
2020-10-23 08:24:10 +0200

    gelöscht

--- aernauticaimperialis/Program.cs
+++ aernauticaimperialis/Program.cs
@@ -6,11 +6,7 @@ namespace aernauticaimperialis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("hallo hi");
-            Console.WriteLine("hallo");
-            Console.WriteLine("tschüss");
-            Console.WriteLine("fnklfsöd");
+            
         }
     }
 }
