bb28b6a512c79458ae98a4743ed48cdc632f8393
thom <t.lueger@htlkrems.at>
2020-10-30 18:42:09 +0100

    Paar Klassen (30.10.2020)

--- aernauticaimperialis/AUnit.cs
+++ aernauticaimperialis/AUnit.cs
@@ -1,5 +1,13 @@
 ﻿namespace aernauticaimperialis {
     public class AUnit {
-        
+        protected int _structure;
+        protected int _speed;
+        protected int _throttle;
+        protected int _minSpeed;
+        protected int _maxSpeed;
+        protected int _maneuver;
+        protected int _handling;
+        protected int _maxAltitude;
+        protected int _altitude;
     }
 }
