a1e39f7890509b13f80f59ffd699af51c567000f
thom <t.lueger@htlkrems.at>
2020-11-02 16:48:57 +0100

    02.11.2020

---
+++ aernauticaImperialis.unitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+using System.Security.Principal;
+using NUnit.Framework;
+
+namespace aernauticaImperialis.unitTest {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+        
+    }
+}
--- aernauticaimperialis/Point.cs
+++ aernauticaimperialis/Point.cs
@@ -1,7 +1,34 @@
-﻿namespace aernauticaimperialis {
+﻿using System;
+
+namespace aernauticaimperialis {
     public class Point {
         protected int _x;
         protected int _y;
         protected int _z;
+        
+        public int X {
+            get => _x;
+            set => _x = value;
+        }
+        public int Y {
+            get => _y;
+            set => _y = value;
+        }
+        public int Z {
+            get => _z;
+            set => _z = value;
+        }
+        
+        public Point(int x, int y, int z) {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public bool IsPointLegal(Point p) {
+
+
+            return true;
+        }
     }
 }
