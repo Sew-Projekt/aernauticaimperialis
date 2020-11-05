7b0e30c661b5a7bc8317d03fa4e26b161b793842
Julia <j.schenk@htlkrems.at>
2020-11-05 17:21:03 +0100

    5.11. weapons, aircrafts

--- aernauticaimperialis/AircraftFactory.cs
+++ aernauticaimperialis/AircraftFactory.cs
@@ -1,14 +1,55 @@
-﻿namespace aernauticaimperialis {
+﻿using System.Linq;
+
+namespace aernauticaimperialis {
     public class AircraftFactory {
         public static Aircraft CreateBigBurna(Point p) {
             Aircraft a = new Aircraft(p.X, p.Y, p.Z, 3, 2, 3, 7, 4, 4,4, 22);
             a.Weapons.Add(WeaponFactory.CreateQuadBigShootas());
+            a.Weapons.Add(WeaponFactory.CreateTurretBigShootas());
+            a.Weapons.Add(WeaponFactory.CreateTailGun());
             return a;
         }
 
         public static Aircraft Vulture(Point p) {
             Aircraft a = new Aircraft(p.X, p.Y, p.Z, 2, 2, 3, 8, 5, 3, 4, 23);
             a.Weapons.Add(WeaponFactory.CreateQuadBigShootas());
+            return a;
+        }
+
+        public static Aircraft GrotBommer(Point p)
+        {
+            Aircraft a = new Aircraft(p.X, p.Y, p.Z, 6, 1, 2, 4, 3, 5, 4, 28);
+            a.Weapons.Add(WeaponFactory.CreateQuadBigShootas());
+            a.Weapons.Add(WeaponFactory.CreatePortTurret());
+            a.Weapons.Add(WeaponFactory.CreateStarbordTurret());
+
+            return a;
+        }
+
+        public static Aircraft BlueDevil(Point p)
+        {
+            Aircraft a = new Aircraft(p.X, p.Y, p.Z, 5, 1, 2, 5, 3, 3, 5, 26);
+            a.Weapons.Add(WeaponFactory.CreateLascannon());
+            a.Weapons.Add(WeaponFactory.CreateDorsalTurret());
+            a.Weapons.Add(WeaponFactory.CreateRearTurret());
+            a.Weapons.Add(WeaponFactory.CreateBombBay());
+
+            return a;
+        }
+
+        public static Aircraft Hellion(Point p)
+        {
+            Aircraft a = new Aircraft(p.X, p.Y, p.Z, 2, 3, 2, 8, 7, 2, 5, 26);
+            a.Weapons.Add(WeaponFactory.CreateTwinMultiLasers());
+            return a;
+        }
+
+        public static Aircraft Executioner(Point p)
+        {
+            Aircraft a = new Aircraft(p.X, p.Y, p.Z, 3, 2, 2, 7, 6, 3, 5, 23);
+            a.Weapons.Add(WeaponFactory.CreateQuadAutocannon());
+            a.Weapons.Add(WeaponFactory.CreateTwinLascannon());
+
             return a;
         }
     }
--- aernauticaimperialis/WeaponFactory.cs
+++ aernauticaimperialis/WeaponFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace aernauticaimperialis {
     public class WeaponFactory {
@@ -40,6 +41,121 @@ namespace aernauticaimperialis {
                 new List<EWeaponOrientationType>() {EWeaponOrientationType.REAR});
 
             return tailgun;
+        }
+
+        public static Weapon CreatePortTurret()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 2;
+            distances[EDistanceType.MEDIUM] = 1;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(5, distances, new List<EWeaponOrientationType>() {EWeaponOrientationType.LEFT, EWeaponOrientationType.UP});
+
+            return weapon;
+        }
+
+        public static Weapon CreateStarbordTurret()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 2;
+            distances[EDistanceType.MEDIUM] = 1;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(5, distances, new List<EWeaponOrientationType>() {EWeaponOrientationType.RIGHT, EWeaponOrientationType.UP});
+
+            return weapon;
+        }
+
+        public static Weapon CreateLascannon()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 0;
+            distances[EDistanceType.MEDIUM] = 2;
+            distances[EDistanceType.LONG] = 1;
+            
+            Weapon weapon = new Weapon(2, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.FRONT});
+
+            return weapon;
+        }
+
+        public static Weapon CreateDorsalTurret()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 3;
+            distances[EDistanceType.MEDIUM] = 2;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(5, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.ALLROUND, EWeaponOrientationType.UP});
+
+            return weapon;
+        }
+
+        public static Weapon CreateRearTurret()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 3;
+            distances[EDistanceType.MEDIUM] = 2;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(5, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.REAR});
+
+            return weapon;
+        }
+
+        public static Weapon CreateBombBay()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 8;
+            distances[EDistanceType.MEDIUM] = 0;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(2, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.REAR});
+
+            return weapon;
+        }
+
+        public static Weapon CreateTwinMultiLasers()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 4;
+            distances[EDistanceType.MEDIUM] = 6;
+            distances[EDistanceType.LONG] = 2;
+            
+            Weapon weapon = new Weapon(5, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.FRONT});
+
+            return weapon;
+        }
+
+        public static Weapon CreateQuadAutocannon()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 2;
+            distances[EDistanceType.MEDIUM] = 6;
+            distances[EDistanceType.LONG] = 0;
+            
+            Weapon weapon = new Weapon(4, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.FRONT});
+
+            return weapon;
+        }
+
+        public static Weapon CreateTwinLascannon()
+        {
+            Dictionary<EDistanceType, int> distances = new Dictionary<EDistanceType, int>();
+            distances[EDistanceType.SHORT] = 0;
+            distances[EDistanceType.MEDIUM] = 2;
+            distances[EDistanceType.LONG] = 1;
+            
+            Weapon weapon = new Weapon(2, distances,
+                new List<EWeaponOrientationType>() {EWeaponOrientationType.FRONT});
+
+            return weapon;
         }
     }
 }
